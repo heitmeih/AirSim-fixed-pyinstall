b6775141d719db509d07e0e27d589b2ab9b709c3
heitmeih <harry.heitmeier@gmail.com>
2023-10-03 19:47:57 -0500

    PythonClient: fix ModuleNotFound exception during installation

--- PythonClient/airsim/__init__.py
+++ PythonClient/airsim/__init__.py
@@ -1,5 +1,9 @@
-from .client import *
-from .utils import *
-from .types import *
+import sys
+
+# block imports during installation
+if "setuptools" not in sys.modules:
+    from .client import *
+    from .utils import *
+    from .types import *
 
 __version__ = "1.8.1"
